40b16239a76e02b3b536869a6d41205b26a113d2
VanHakobyan <van19962013@mail.ru>
2017-02-02 18:49:14 +0400

    add methods

--- Chess8Queen/Chess8Queen/Program.cs
+++ Chess8Queen/Chess8Queen/Program.cs
@@ -11,81 +11,7 @@ namespace Chess8Queen
     class Program
     {
         const int N = 8;
-        public static bool SolutionCheck(int[,] board)
-        {
-
-
-            //first
-            for (int i = 0; i < N; i++)
-            {
-                int sum = 0;
-                for (int j = 0; j < N; j++)
-                {
-                    sum = sum + board[i, j];
-                }
-                if (sum > 1)
-                {
-                    return false;
-                }
-            }
-
-
-            //second
-            for (int i = 0, j = N - 2; j >= 0; j--)
-            {
-                int sum = 0;
-                for (int p = i, q = j; q < N; p++, q++)
-                {
-                    sum = sum + board[p, q];
-                }
-                if (sum > 1)
-                {
-                    return false;
-                }
-            }
-            //third
-            for (int i = 1, j = 0; i < N - 1; i++)
-            {
-                int sum = 0;
-                for (int p = i, q = j; p < N; p++, q++)
-                {
-                    sum = sum + board[p, q];
-                }
-                if (sum > 1)
-                {
-                    return false;
-                }
-            }
-            //fourth
-            for (int i = 0, j = 1; j < N; j++)
-            {
-                int sum = 0;
-                for (int p = i, q = j; q >= 0; p++, q--)
-                {
-                    sum = sum + board[p, q];
-                }
-                if (sum > 1)
-                {
-                    return false;
-                }
-            }
-
-
-            //fifth
-            for (int i = 1, j = N - 1; i < N - 1; i++)
-            {
-                int sum = 0;
-                for (int p = i, q = j; p < N; p++, q--)
-                {
-                    sum = sum + board[p, q];
-                }
-                if (sum > 1)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+      
 
         static void Main(string[] args)
         {
@@ -131,7 +57,7 @@ namespace Chess8Queen
                     {
                         board[pointer[j] - 1, j] = 0;
                     }
-                    if (SolutionCheck(board))
+                    if (methods.SolutionCheck(board))
                     {
                         j++;
                         if (j == N)
---
+++ Chess8Queen/Chess8Queen/methods.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess8Queen
+{
+    public static class methods
+    {
+        const int N = 8;
+        public static bool SolutionCheck(int[,] board)
+        {
+
+
+            //first
+            for (int i = 0; i < N; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < N; j++)
+                {
+                    sum = sum + board[i, j];
+                }
+                if (sum > 1)
+                {
+                    return false;
+                }
+            }
+
+
+            //second
+            for (int i = 0, j = N - 2; j >= 0; j--)
+            {
+                int sum = 0;
+                for (int p = i, q = j; q < N; p++, q++)
+                {
+                    sum = sum + board[p, q];
+                }
+                if (sum > 1)
+                {
+                    return false;
+                }
+            }
+            //third
+            for (int i = 1, j = 0; i < N - 1; i++)
+            {
+                int sum = 0;
+                for (int p = i, q = j; p < N; p++, q++)
+                {
+                    sum = sum + board[p, q];
+                }
+                if (sum > 1)
+                {
+                    return false;
+                }
+            }
+            //fourth
+            for (int i = 0, j = 1; j < N; j++)
+            {
+                int sum = 0;
+                for (int p = i, q = j; q >= 0; p++, q--)
+                {
+                    sum = sum + board[p, q];
+                }
+                if (sum > 1)
+                {
+                    return false;
+                }
+            }
+
+
+            //fifth
+            for (int i = 1, j = N - 1; i < N - 1; i++)
+            {
+                int sum = 0;
+                for (int p = i, q = j; p < N; p++, q--)
+                {
+                    sum = sum + board[p, q];
+                }
+                if (sum > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
