1931fb331333a7f12d2ec9f327e60e55a9875953
VanHakobyan <van19962013@mail.ru>
2017-02-02 18:46:05 +0400

    add main

--- Chess8Queen/Chess8Queen/Program.cs
+++ Chess8Queen/Chess8Queen/Program.cs
@@ -13,7 +13,7 @@ namespace Chess8Queen
         const int N = 8;
         public static bool SolutionCheck(int[,] board)
         {
-           
+
 
             //first
             for (int i = 0; i < N; i++)
@@ -28,7 +28,7 @@ namespace Chess8Queen
                     return false;
                 }
             }
-           
+
 
             //second
             for (int i = 0, j = N - 2; j >= 0; j--)
@@ -84,18 +84,73 @@ namespace Chess8Queen
                     return false;
                 }
             }
-
-
-
-
             return true;
         }
 
-
-
         static void Main(string[] args)
         {
-            
+            int count = 0;
+            int[,] board = new int[N, N];
+
+            //array to 0
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    board[i, j] = 0;
+                }
+            }
+
+            // pointer array
+            int[] pointer = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                pointer[i] = -1;
+            }
+
+            //Implementation
+            for (int j = 0; ;)
+            {
+                pointer[j]++;
+                
+                if (pointer[j] == N)
+                {
+                    board[pointer[j] - 1, j] = 0;
+                    pointer[j] = -1;
+                    j--;
+                    if (j == -1)
+                    {
+                        Console.WriteLine("All have been examined...");
+                        break;
+                    }
+                }
+                else
+                {
+                    board[pointer[j], j] = 1;
+                    if (pointer[j] != 0)
+                    {
+                        board[pointer[j] - 1, j] = 0;
+                    }
+                    if (SolutionCheck(board))
+                    {
+                        j++;
+                        if (j == N)
+                        {
+                            j--;
+                            count++;
+                            Console.WriteLine("Solution" + count.ToString() + ":");
+                            for (int p = 0; p < N; p++)
+                            {
+                                for (int q = 0; q < N; q++)
+                                {
+                                    Console.Write(board[p, q] + " ");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+            }
         }
 
     }
